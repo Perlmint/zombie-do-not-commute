93b68e47bc4b8839ce8d765c62cb23ea73259027
Gyusun Yeom <omniavinco@gmail.com>
2019-05-08 00:21:00 +0900

    Add message to spinner

--- client/src/spinner.tsx
+++ client/src/spinner.tsx
@@ -3,15 +3,18 @@ import "./spinner.less";
 import React from "react";
 
 export function Spinner() {
-    return <div className="lds-grid">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    return <div className="spinner-wrap">
+        <div className="lds-grid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <h2>LOADING...</h2>
     </div>;
 }
